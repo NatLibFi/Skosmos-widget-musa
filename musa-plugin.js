df11221b2d3b79c63386b5dd810f14cf5d242954
Henri Ylikotila <henri.ylikotila@helsinki.fi>
2017-11-07 11:58:41 +0200

    päivitetään käännöstä

--- musa-plugin.js
+++ musa-plugin.js
@@ -9,7 +9,7 @@ MUSA = {
     },
     warning: {
         'fi': ['Musiikin asiasanasto MUSA yhdistetään Yleiseen suomalaiseen asiasanastoon YSAan loppuvuoden 2017 aikana. MUSAa ei enää päivitetä. Uusia musiikkitermejä voi ehdottaa', 'YSAn ehdotusjärjestelmällä', '. MUSA poistuu käytöstä itsenäisenä sanastona vuoden 2018 alussa. Ruotsinkielinen musiikin asiasanasto CILLA yhdistetään vastaavasti Allärsiin.'],
-        'sv': ['CILLA - Specialtesaurus för musik uppgår i Allärs - Allmän tesaurus på svenska i slutet av år 2017. Cilla uppdateras inte längre. Förslag till nya musiktermer kan ges via', 'blanketten för begreppsförslag till Allärs och ALLFO', '. Cilla upphör som separat tesaurus i början av år 2018. Den finskspråkiga musiktesaurusen MUSA uppgår på motsvarande sätt i YSA.']
+        'sv': ['CILLA - Specialtesaurus för musik uppgår i Allärs - Allmän tesaurus på svenska under slutet av 2017. Cilla uppdateras inte längre. Förslag till nya musiktermer kan ges via', 'blanketten för begreppsförslag till Allärs och ALLFO', '. Cilla upphör som separat tesaurus i början av år 2018. Den finskspråkiga musiktesaurusen MUSA uppgår på motsvarande sätt i YSA.']
     },
     link: "http://ehdotus.finto.fi/#/new",
     renderWarning: function(params) {
