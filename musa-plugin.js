9f0d6f38c2e8dfe54d6b7d997686511b25fb388b
kouralex <1723419+kouralex@users.noreply.github.com>
2018-07-30 10:19:27 +0300

    Päivitin Katerinan pyynnöstä näytettävää tekstiä

--- musa-plugin.js
+++ musa-plugin.js
@@ -8,10 +8,10 @@ MUSA = {
       'en': 'NB:'
     },
     warning: {
-        'fi': ['Musiikin asiasanasto MUSA yhdistetään Yleiseen suomalaiseen asiasanastoon YSAan loppuvuoden 2017 aikana. MUSAa ei enää päivitetä. Uusia musiikkitermejä voi ehdottaa', 'YSAn ehdotusjärjestelmällä', '. MUSA poistuu käytöstä itsenäisenä sanastona vuoden 2018 alussa. Ruotsinkielinen musiikin asiasanasto CILLA yhdistetään vastaavasti Allärsiin.'],
-        'sv': ['CILLA - Specialtesaurus för musik har uppgått i Allärs - Allmän tesaurus på svenska och den finskspråkiga musiktesaurusen MUSA har på motsvarande sätt uppgått i YSA. Förslag till nya musiktermer kan ges i', 'Allärs förslagssystem', '. Termer i CILLA uppdateras inte längre. För beskrivning av musiklitteratur används Allärs-termer. CILLA upphör efter att Finländsk genre- och formlista publiceras.']
+        'fi': ['Musiikin asiasanasto MUSA on yhdistetty Yleiseen suomalaiseen asiasanastoon YSAan ja ruotsinkielinen musiikin asiasanasto CILLA vastaavasti Allärsiin. Uusia musiikkitermejä voi ehdottaa', 'YSAn ehdotusjärjestelmällä', '. Tässä sanastossa olevia termejä ei enää päivitetä. Musiikkikirjallisuuden kuvailussa käytetään YSAn termejä. MUSA poistuu käytöstä itsenäisenä sanastona viimeistään siinä vaiheessa, kun YSA yhdistetään YSOon (kesällä 2019).'],
+        'sv': ['CILLA - Specialtesaurus för musik har uppgått i Allärs - Allmän tesaurus på svenska och den finskspråkiga musiktesaurusen MUSA har på motsvarande sätt uppgått i YSA. Förslag till nya musiktermer kan ges i', 'Allärs förslagssystem', '. Termer i CILLA uppdateras inte längre. För beskrivning av musiklitteratur används Allärs-termer. CILLA läggs ned senast då Allärs sammanslås ned ALLFO (sommaren 2019).']
     },
-    link: "http://ehdotus.finto.fi/#/new",
+    link: "http://ehdotus.finto.fi/#!/list#%2Fnew",
     renderWarning: function(params) {
         $('.content').prepend(Handlebars.compile($('#musa-template').html())({
             'warning-first': this.warning[lang][0], 
