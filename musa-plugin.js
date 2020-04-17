bc00c2412d3caf0594a460a00b7d5e03bc5b0a3a
tpalonen <tuomas.palonen@helsinki.fi>
2020-04-17 14:57:53 +0300

    ajantasaistettu suomeksi teksti

--- musa-plugin.js
+++ musa-plugin.js
@@ -8,7 +8,7 @@ MUSA = {
       'en': 'NB:'
     },
     warning: {
-        'fi': ['Musiikin asiasanasto MUSA on yhdistetty Yleiseen suomalaiseen asiasanastoon YSAan ja ruotsinkielinen musiikin asiasanasto CILLA vastaavasti Allärsiin. Uusia musiikkitermejä voi ehdottaa', 'YSAn ehdotusjärjestelmällä', '. Tässä sanastossa olevia termejä ei enää päivitetä. Musiikkikirjallisuuden kuvailussa käytetään YSAn termejä. MUSA poistuu käytöstä itsenäisenä sanastona viimeistään siinä vaiheessa, kun YSA yhdistetään YSOon (kesällä 2019).'],
+        'fi': ['Musiikin asiasanasto MUSA on poistunut käytöstä itsenäisenä sanastona. Sanastoa ei enää ylläpidetä eikä sen termejä päivitetä. Musiikkikirjallisuuden kuvailussa käytetään YSOn käsitteitä. Uusia musiikkialan termejä voi ehdottaa YSOon ', 'ehdotusjärjestelmällä', '.'],
         'sv': ['CILLA - Specialtesaurus för musik har uppgått i Allärs - Allmän tesaurus på svenska och den finskspråkiga musiktesaurusen MUSA har på motsvarande sätt uppgått i YSA. Förslag till nya musiktermer kan ges i', 'Allärs förslagssystem', '. Termer i CILLA uppdateras inte längre. För beskrivning av musiklitteratur används Allärs-termer. CILLA läggs ned senast då Allärs sammanslås ned ALLFO (sommaren 2019).']
     },
     link: "http://ehdotus.finto.fi/#!/list#%2Fnew",
